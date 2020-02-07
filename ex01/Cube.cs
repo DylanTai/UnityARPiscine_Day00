d6395d404f14bd3409a92bfa7c09a7226df27889
Dylan Tai <dylztai@gmail.com>
2020-02-06 19:02:57 -0800

    ex01

---
+++ ex01/Cube.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private float type;
+    private int speed;
+    [SerializeField] private int min_speed;
+    [SerializeField] private int max_speed;
+    [SerializeField] private float margin_of_error;
+    [SerializeField] GameObject a_detection;
+    [SerializeField] GameObject s_detection;
+    [SerializeField] GameObject d_detection;
+    private GameObject line;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameObject.Find("line"))
+            line = GameObject.Find("line");
+        type = Random.Range(0, 3);
+        if (type == 0)
+            GetComponent<SpriteRenderer>().color = new Color (255, 0, 0, 255);
+        else if (type == 1)
+            GetComponent<SpriteRenderer>().color = new Color (0, 66, 255, 255);
+        else if (type == 2)
+            GetComponent<SpriteRenderer>().color = new Color (205, 255, 0, 255);
+        speed = Random.Range(min_speed, max_speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float pos_x = 0f;
+        if (type == 0)
+            pos_x = -2.96f;
+        else if (type == 2)
+            pos_x = 2.96f;
+        float dist = this.gameObject.transform.position.y;
+        float pos_y = dist - speed/1000f;
+        this.gameObject.transform.position = new Vector3(pos_x, pos_y, -1);
+        if (Input.GetKeyDown("a") && type == 0)
+        {
+            dist -= a_detection.gameObject.transform.position.y;
+            Debug.Log("Precision: " + dist * 100 + "\n");
+            if (dist <= margin_of_error && dist >= margin_of_error * -1)
+            {
+                line.GetComponent<CubeSpawner>().score++;
+                Destroy(this.gameObject);
+            }
+        }
+        if (Input.GetKeyDown("s") && type == 1)
+        {
+            dist -= s_detection.gameObject.transform.position.y;
+            Debug.Log("Precision: " + dist * 100 + "\n");
+            if (dist <= margin_of_error && dist >= margin_of_error * -1)
+            {
+                line.GetComponent<CubeSpawner>().score++;
+                Destroy(this.gameObject);
+            }
+        }
+        if (Input.GetKeyDown("d") && type == 2)
+        {
+            dist -= d_detection.gameObject.transform.position.y;
+            Debug.Log("Precision: " + dist * 100 + "\n");
+            if (dist <= margin_of_error && dist >= margin_of_error * -1)
+            {
+                line.GetComponent<CubeSpawner>().score++;
+                Destroy(this.gameObject);
+            }
+        }
+        if (dist <= -5)
+            Destroy(this.gameObject);
+    }
+}
---
+++ ex01/CubeSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CubeSpawner : MonoBehaviour
+{
+    private int time;
+    [SerializeField] private int min_spawntime;
+    [SerializeField] private int max_spawntime;
+    [SerializeField] GameObject obj_Cube;
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        score = 0;
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("r"))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        if (time <= 0)
+            time = Random.Range(min_spawntime, max_spawntime);
+        time -= 1;
+        if (time <= 0)
+            Instantiate(obj_Cube);
+    }
+}
