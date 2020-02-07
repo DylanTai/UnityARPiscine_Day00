3dd1c3fef55d14c048fa34d8738618bd95e40352
Dylan Tai <dylztai@gmail.com>
2020-02-06 19:09:35 -0800

    ex03_extra

---
+++ ex03_extra/Bird_extra.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bird_extra : MonoBehaviour
+{
+    public int score;
+    public bool dead;
+    private int speed;
+    [SerializeField] float grav_multi;
+    [SerializeField] int jump_amt;
+    private bool destroyed;
+    private bool displayed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        score = 0;
+        dead = false;
+        speed = 0;
+        destroyed = false;
+        displayed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("r"))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        if (!destroyed)
+        {
+            Vector3 this_pos = this.gameObject.transform.position;
+            this.transform.Rotate(0, 0, speed / 3f, Space.Self);
+            if (!dead && Input.GetKeyDown("space"))
+            {
+                this.transform.Rotate(0, 0, (jump_amt - speed) / 3f, Space.Self);
+                speed = jump_amt;
+            }
+            this.gameObject.transform.position = new Vector3(this_pos.x, this_pos.y + grav_multi * speed, this_pos.z);
+            if (this_pos.y < -4.4f || this_pos.y > 6.4f)
+                dead = true;
+            speed--;
+            if (this_pos.y < -5.2f)
+                destroyed = true;
+        }
+        else if (!displayed)
+        {
+            Debug.Log("Score: " + score + "\nTime: " + Mathf.RoundToInt(Time.realtimeSinceStartup) + "s\n");
+            displayed = true;
+        }
+    }
+}
---
+++ ex03_extra/Pipe_extra.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+Measurements:
+spawn = 9.7 x
+despawn = -11.38 x
+lowest pipe = -1.2 y
+highest pipe = 3.3 y
+hitbox = (-1.34x to 1.34x) (-1.48y to 1.1y)
+*/
+
+public class Pipe_extra : MonoBehaviour
+{
+    private GameObject obj_bird;
+    [SerializeField] GameObject obj_pipe;
+    [SerializeField] float spawn_x;
+    [SerializeField] float despawn_x;
+    [SerializeField] float spawn_y_low;
+    [SerializeField] float spawn_y_high;
+    private float spawn_y;
+    [SerializeField] float hb_left;
+    [SerializeField] float hb_right;
+    [SerializeField] float hb_down;
+    [SerializeField] float hb_up;
+    [SerializeField] float speed_pipe;
+    private bool added_point;
+    [SerializeField] float difficulty;
+    private bool added_pipe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawn_y = Random.Range(spawn_y_low, spawn_y_high);
+        this.gameObject.transform.position = new Vector3(spawn_x, spawn_y, 0);
+        if (GameObject.Find("bird"))
+            obj_bird = GameObject.Find("bird");
+        added_point = false;
+        added_pipe = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool stop = obj_bird.GetComponent<Bird_extra>().dead;
+        Vector3 this_pos = this.gameObject.transform.position;
+        if (!stop)
+        {
+            this.gameObject.transform.position = new Vector3(this_pos.x - speed_pipe * (obj_bird.GetComponent<Bird_extra>().score * difficulty / 10 + 1), this_pos.y, 0);
+            if (!added_pipe && this_pos.x <= 4.8 && this_pos.x <= 5.2)
+            {
+                added_pipe = true;
+                Instantiate(obj_pipe);
+            }
+            if (this_pos.x <= -1.34 && !added_point)
+            {
+                obj_bird.GetComponent<Bird_extra>().score += 5;
+                added_point = true;
+            }
+            Vector3 bird_pos = obj_bird.gameObject.transform.position;
+            if (bird_pos.x >= this_pos.x + hb_left && bird_pos.x <= this_pos.x + hb_right &&
+                    !(bird_pos.y >= this_pos.y + hb_down && bird_pos.y <= this_pos.y + hb_up))
+                obj_bird.GetComponent<Bird_extra>().dead = true;
+            if (this_pos.x <= despawn_x)
+                Destroy(gameObject);
+        }
+    }
+}
