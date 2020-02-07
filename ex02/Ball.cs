cace43b1ccfe466649c079a18e47fb76b494b320
Dylan Tai <dylztai@gmail.com>
2020-02-06 19:07:34 -0800

    ex02

---
+++ ex02/Ball.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour
+{
+    private GameObject obj_club;
+    public int speed;
+    public bool destroyed;
+    public int direction;
+    [SerializeField] GameObject obj_lwall;
+    [SerializeField] GameObject obj_rwall;
+    [SerializeField] GameObject obj_hole;
+    // Start is called before the first frame update
+    [SerializeField] float speed_divdr;
+    [SerializeField] float margin_of_error;
+    [SerializeField] int speed_threshold;
+
+
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        if (GameObject.Find("club"))
+            obj_club = GameObject.Find("club");
+        speed = 0;
+        destroyed = false;
+        direction = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("r"))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        if (!destroyed)
+        {
+            if (speed > 0)
+            {
+                Vector3 this_pos = this.gameObject.transform.position;
+                if (this_pos.x <= obj_lwall.gameObject.transform.position.x + 1.5)
+                    direction = 1;
+                else if (this_pos.x >= obj_rwall.gameObject.transform.position.x - 1.5)
+                    direction = -1;
+                this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x + speed/speed_divdr * direction, 1, -0.01f);
+                speed -= 1;
+                if (this_pos.x >= obj_hole.gameObject.transform.position.x - margin_of_error && this_pos.x <= obj_hole.gameObject.transform.position.x + margin_of_error && speed <= speed_threshold)
+                    destroyed = true;
+            }
+        }
+        else
+        {
+            if (this.gameObject.transform.localScale.x > 0)
+            {
+                Vector3 this_scale = this.gameObject.transform.localScale;
+                this.gameObject.transform.localScale = new Vector3(this_scale.x - 0.01f, this_scale.y - 0.01f, this_scale.z);
+            }
+            else
+                this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+}
---
+++ ex02/Club.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Club : MonoBehaviour
+{
+    private int score;
+    [SerializeField] GameObject obj_ball;
+    [SerializeField] GameObject obj_hole;
+    private Vector3 reset_pos;
+    private int power;
+    private bool hidden;
+    private bool incrementing;
+    [SerializeField] int max_power;
+    [SerializeField] int increment_power;
+    public int pos_multi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = -15;
+        power = 0;
+        hidden = false;
+        incrementing = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!obj_ball.GetComponent<Ball>().destroyed)
+        {
+            Vector3 ball_pos = obj_ball.gameObject.transform.position;
+            reset_pos = new Vector3(ball_pos.x + pos_multi * 1f, ball_pos.y + 0.5f, 0.01f);
+            Vector3 hole_pos = obj_hole.gameObject.transform.position;
+            if (ball_pos.x < hole_pos.x)
+                pos_multi = -1;
+            else
+                pos_multi = 1;
+            if (obj_ball.GetComponent<Ball>().speed > 0 || obj_ball.GetComponent<Ball>().destroyed)
+            {
+                hidden = true;
+                this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            }
+            else
+            {
+                hidden = false;
+                this.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+            }
+            if (!hidden && !Input.GetKey("space"))
+            {
+                if (power != 0)
+                {
+                    obj_ball.GetComponent<Ball>().speed = power;
+                    obj_ball.GetComponent<Ball>().direction = pos_multi * -1;
+                    incrementing = true;
+                    power = 0;
+                    score += 5;
+                }
+                this.gameObject.transform.position = reset_pos;
+            }
+            else if (!hidden && Input.GetKey("space"))
+            {
+                if (incrementing && power < max_power)
+                    power += increment_power;
+                else if (incrementing)
+                    incrementing = false;
+                if (!incrementing && power > 0)
+                    power -= increment_power;
+                else if (!incrementing)
+                    incrementing = true;
+                this.gameObject.transform.position = new Vector3(ball_pos.x + pos_multi * (1f + power/42f), ball_pos.y + 0.5f, 0.01f);
+            }
+        }
+        else
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            Debug.Log("Score: " + score);
+        }
+    }
+}
